db23f51a0cf62fa7a1e32d7f791aa6a884772d9d
RMED178LT <jkdesignlab@gmail.com>
2015-07-27 11:03:33 +0530

    rspec: tested calculate_total_experience

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -7,7 +7,20 @@ FactoryGirl.define do
 		f.password "password"
 		f.password_confirmation "password"
 		f.previous_experience 1.0
-		f.date_of_joining Time.now
+		f.date_of_joining '2014-07-27'
+		f.status "Active"
 		f.work_phone "180-111-222-3333"
 	end 
+	# factory :Lockeduser do |f| 
+	# 	f.first_name "Mathew" 
+	# 	f.last_name "Thomas" 
+	# 	f.email "mathewthomas@yahoo.com"
+	# 	f.login_id "mathewthomas"
+	# 	f.password "password"
+	# 	f.password_confirmation "password"
+	# 	f.previous_experience 2.0
+	# 	f.status "Locked"
+	# 	f.date_of_joining Time.now
+	# 	f.work_phone "180-111-222-3333"
+	# end 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,19 @@
 require 'spec_helper'
 describe User do
+	# before(:user) do
+ #    @thing = Thing.new
+ #  end
 	#it "gives total experience of a user"
   it "has a valid factory" do 
   	@user = FactoryGirl.create(:user)
   	expect(@user).to be_valid
   end
+  context "calculate_total_experience" do
+  	it "gives total experience" do
+  	  @user = FactoryGirl.create(:user)
+
+  	  expect(@total_experience = @user.calculate_total_experience).to eq(2.0)
+  	end
+  end
+
 end
