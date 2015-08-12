8c35ba9799005f02af66c49497254721dc8cbcf4
Jayakrishnan Gopal <jkdesignlab@gmail.com>
2015-08-12 16:00:34 +0530

    images deleted

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,17 +2,22 @@ require 'rails_helper'
 RSpec.describe "Users", type: :request do
   before (:each) do
     @role = FactoryGirl.create(:admin)
-    @designation = FactoryGirl.create(:designation , designation_name: "Trainee")
+    @designation = FactoryGirl.create(:designation)
     @user = FactoryGirl.create(:admin_user, role: @role, designation: @designation)
-  end
-  it "signs me in" do
     visit new_user_session_path
     within("#session") do
       fill_in 'Email', :with => 'mathewthomas@gmail.com'
       fill_in 'Password', :with => 'password'
     end
     click_button 'Log in'
+  end
+  it "signs me in" do
     save_and_open_page
     expect(page).to have_content 'ADMIN PAGE'
+  end
+  it "creates a user" do
+    click_link 'New User'
+    save_and_open_page
+    expect(page).to have_content 'NEW USER'
   end
 end
