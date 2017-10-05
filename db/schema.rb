ca60b33d381e910305b217f320aae4fb2fa85bfa
Arun Mathew Kurian <amkurian@gmail.com>
2017-10-05 19:16:48 +0530

    Rails 5 upgrade updated gem file and configs

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,102 +10,101 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170218102054) do
+ActiveRecord::Schema.define(version: 20170219093204) do
 
-  create_table "admins", force: :cascade do |t|
-    t.string   "name",                   limit: 255
-    t.string   "admin_id",               limit: 255
-    t.datetime "created_at",                                      null: false
-    t.datetime "updated_at",                                      null: false
-    t.string   "email",                  limit: 255, default: "", null: false
-    t.string   "encrypted_password",     limit: 255, default: "", null: false
-    t.string   "reset_password_token",   limit: 255
+  create_table "admins", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
+    t.string   "name"
+    t.string   "admin_id"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",     limit: 255
-    t.string   "last_sign_in_ip",        limit: 255
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.index ["email"], name: "index_admins_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true, using: :btree
   end
 
-  add_index "admins", ["email"], name: "index_admins_on_email", unique: true, using: :btree
-  add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true, using: :btree
-
-  create_table "bid_logs", force: :cascade do |t|
-    t.integer  "amount",     limit: 4
-    t.integer  "user_id",    limit: 4
-    t.datetime "created_at",           null: false
-    t.datetime "updated_at",           null: false
-    t.integer  "player_id",  limit: 4
+  create_table "bid_logs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
+    t.integer  "amount"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "player_id"
+    t.boolean  "is_closed"
+    t.index ["user_id"], name: "index_bid_logs_on_user_id", using: :btree
   end
 
-  add_index "bid_logs", ["user_id"], name: "index_bid_logs_on_user_id", using: :btree
-
-  create_table "designations", force: :cascade do |t|
-    t.string   "designation_name", limit: 255
-    t.datetime "created_at",                   null: false
-    t.datetime "updated_at",                   null: false
+  create_table "designations", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
+    t.string   "designation_name"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
   end
 
-  create_table "roles", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+  create_table "roles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "teams", force: :cascade do |t|
-    t.string  "name",        limit: 255
-    t.integer "purse_spent", limit: 4
-    t.integer "total_purse", limit: 4
+  create_table "teams", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
+    t.string  "name"
+    t.integer "purse_spent"
+    t.integer "total_purse"
+    t.integer "owner_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.integer  "designation_id",         limit: 4
-    t.string   "first_name",             limit: 255
-    t.string   "middle_name",            limit: 255
-    t.string   "last_name",              limit: 255
-    t.string   "email",                  limit: 255
-    t.string   "login_id",               limit: 255
-    t.string   "employee_id",            limit: 255
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
+    t.integer  "designation_id"
+    t.string   "first_name"
+    t.string   "middle_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "login_id"
+    t.string   "employee_id"
     t.date     "date_of_birth"
-    t.string   "gender",                 limit: 255
-    t.string   "time_zone",              limit: 255
+    t.string   "gender"
+    t.string   "time_zone"
     t.date     "date_of_joining"
     t.text     "education",              limit: 65535
     t.text     "comments",               limit: 65535
     t.boolean  "is_locked"
     t.date     "deactivated_on"
     t.text     "reason",                 limit: 65535
-    t.string   "work_phone",             limit: 255
+    t.string   "work_phone"
     t.datetime "created_at",                                        null: false
     t.datetime "updated_at",                                        null: false
-    t.string   "encrypted_password",     limit: 255,   default: "", null: false
-    t.string   "reset_password_token",   limit: 255
+    t.string   "encrypted_password",                   default: "", null: false
+    t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          limit: 4,     default: 0,  null: false
+    t.integer  "sign_in_count",                        default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",     limit: 255
-    t.string   "last_sign_in_ip",        limit: 255
-    t.integer  "role_id",                limit: 4
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.integer  "role_id"
     t.float    "previous_experience",    limit: 24
-    t.integer  "failed_attempts",        limit: 4,     default: 0
-    t.string   "unlock_token",           limit: 255
+    t.integer  "failed_attempts",                      default: 0
+    t.string   "unlock_token"
     t.datetime "locked_at"
-    t.string   "avatar",                 limit: 255
-    t.string   "status",                 limit: 255
-    t.integer  "base_points",            limit: 4
-    t.integer  "sold_points",            limit: 4
-    t.integer  "team_id",                limit: 4
-    t.string   "team_status",            limit: 255
+    t.string   "avatar"
+    t.string   "status"
+    t.integer  "base_points"
+    t.integer  "sold_points"
+    t.integer  "team_id"
+    t.string   "team_status"
+    t.index ["designation_id"], name: "index_users_on_designation_id", using: :btree
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.index ["role_id"], name: "index_users_on_role_id", using: :btree
   end
-
-  add_index "users", ["designation_id"], name: "index_users_on_designation_id", using: :btree
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-  add_index "users", ["role_id"], name: "index_users_on_role_id", using: :btree
 
   add_foreign_key "bid_logs", "users"
   add_foreign_key "users", "roles"
